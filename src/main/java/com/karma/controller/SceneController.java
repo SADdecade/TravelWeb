a60e2a6e16d65f6f3a06c8d3f862104c119a1a2b
ypxgitdemo <1269874756@qq.com>
2021-09-12 04:09:58 +0800

    Merge branch 'ch2' of github.com:SADdecade/TravelWeb
    # Conflicts:
    #	.idea/TravelWeb.iml

--- src/main/java/com/karma/controller/SceneController.java
+++ src/main/java/com/karma/controller/SceneController.java
@@ -1,9 +1,18 @@
 package com.karma.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.karma.pojo.Scene;
+import com.karma.service.SceneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("/Scene")
@@ -11,9 +20,17 @@ public class SceneController {
 
     @Autowired
     @Qualifier("SceneServiceImpl")
+    private SceneService sceneService;
 
     @RequestMapping("/queryall")
-    public String queryall(){
+    public String queryall(@RequestParam(value = "pn",defaultValue = "1") int pn, Model model){
+        PageHelper.startPage(pn,5);
+        HashMap map = new HashMap();
+        List<Scene> scenes = sceneService.querySceneByMap(map);
+        PageInfo page = new PageInfo(scenes,5);
+
+        model.addAttribute("pageInfo",page);
+
         return "SceneAll";
     }
 }
